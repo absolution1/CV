03d9fc1c2c17ca73f119396df2b24aab36b433ad
Dominic <dominicbrailsford@gmail.com>
2018-01-10 21:20:45 +0000

    Add ignore

--- getBibFromInspires.sh
+++ getBibFromInspires.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "THIS CURRENTLY ALSO SCRAPES A. D. BRAILSFORD PAPER, REMOVE IT MANUALLY!"
-curl 'https://inspirehep.net/search?p=f+a+d+brailsford&of=hx&em=B&sf=year&so=d&rg=250' > CV_bib.bib
-perl -i -pe 's/<\/?\w+>//g;s/<[^<]+pagebody[^>]+>//g' CV_bib.bib
+curl 'https://inspirehep.net/search?p=f+a+d+brailsford&of=hx&em=B&sf=year&so=d&rg=250' > new_publications.bib
+perl -i -pe 's/<\/?\w+>//g;s/<[^<]+pagebody[^>]+>//g' new_publications.bib
